30bfd9c40566744a2268315cacb2e0f20c5199fb
dualsaber <dualsaber88@gmail.com>
2015-03-08 13:20:56 +0200

    General commit

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
-    $conn = mysql_connect('localhost', 'canagirk_arabeni', 'arabenikany10');
-    $db = mysql_select_db('canagirk_arabeni');
+    //$conn = mysql_connect('localhost', 'canagirk_arabeni', 'arabenikany10');
+    //$db = mysql_select_db('canagirk_arabeni');
 
-    //$conn = mysql_connect('localhost', 'root', 'root');
-    //$db = mysql_select_db('arabeni');
+    $conn = mysql_connect('localhost', 'root', 'root');
+    $db = mysql_select_db('arabeni');
 ?>
